26bb0dc1d3c6cb850b2c508ed5f58e2e94f6a0ae
Alvaro Beckett <alvaro@alvarobeckett.com>
2018-02-22 18:51:40 -0500

    First page done

---
+++ assets/javascript/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$(".menu-icon").on("click", function() {
+		$("nav ul").toggleClass("showing");
+	});
+});
+
+$(window).on("scroll", function() {
+	if($(window).scrollTop()) {
+		$('nav').addClass('green');
+		$('.logo a').css({"color": "#fff"})
+		$('nav ul').css({"color": "#fff"})
+		$('nav a').css({"color": "#fff"})
+	}
+	else {
+		$('nav').removeClass('green');
+		$('.logo a').css({"color": "#000"})
+		$('nav ul').css({"color": "#000"})
+		$('nav a').css({"color": "#000"})
+	}
+})
